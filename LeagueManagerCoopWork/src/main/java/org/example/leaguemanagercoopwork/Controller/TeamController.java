715f1a7ea3503a164863cb1ca45cfd14f8ff8eec
Stephen Cooper <scooper@indeed.com>
2024-04-16 17:07:32 -0500

    Added packages and teamrepo

---
+++ LeagueManagerCoopWork/src/main/java/org/example/leaguemanagercoopwork/Controller/TeamController.java
@@ -0,0 +1,4 @@
+package org.example.leaguemanagercoopwork.Controller;
+
+public class TeamController {
+}
---
+++ LeagueManagerCoopWork/src/main/java/org/example/leaguemanagercoopwork/Team/TeamRepository.java
@@ -0,0 +1,7 @@
+package org.example.leaguemanagercoopwork.Team;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
+
